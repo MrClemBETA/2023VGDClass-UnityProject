eac1c60361bb635c5f53f3e2c44144c234e76312
Tyler Lynch <tlynch@cohsbusiness.edu>
2023-11-27 09:52:48 -0800

    Finished party follow script and avoidance of obstacles

---
+++ Assets/Scripts/Character/PartyHealth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyHealth : MonoBehaviour
+{
+    [SerializeField] public int maxLHealth = 15;
+    [SerializeField] public int LHealth = 15;
+    [SerializeField] public int maxMHealth = 12;
+    [SerializeField] public int MHealth = 10;
+}
