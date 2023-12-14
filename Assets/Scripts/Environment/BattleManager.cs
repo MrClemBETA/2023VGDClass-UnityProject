6e29d52c1be01575e629fae2a88f737a98c75e54
Tyler Lynch <tlynch@cohsbusiness.edu>
2023-12-14 10:21:08 -0800

    Worked more on bugfixes

--- Assets/Scripts/Environment/BattleManager.cs
+++ Assets/Scripts/Environment/BattleManager.cs
@@ -11,22 +11,23 @@ namespace SOS.AndrewsAdventure.Character
     /// DD = Defense Divider, the value an attack's total damage is divided by
     /// BD = Bonus Defense, additional defense that isn't affected by the defense divider
     [RequireComponent(typeof(Health))]
-    public class BattleCharacter : MonoBehaviour
+    public class BattleManager : MonoBehaviour
     {
         [SerializeField] public bool inbattle = false;
-        public Transform TheParty;
+        public Transform TheOriginalAndrew;
         public Transform TheFirstOpponent;
         public Transform Meresankh;
         public Transform Lateef;
         public Camera MainCamera;
         public Camera BattleCamera;
-        private NavMeshAgent Enemy;
+        public NavMeshAgent Enemy;
 
         public void Start()
         {
             Enemy = GetComponent<NavMeshAgent>();
             MainCamera.enabled = true;
             BattleCamera.enabled = false;
+            inbattle = false;
         }
         public void OnTriggerEnter(Collider TheFirstOpponent)
         {
@@ -35,14 +36,14 @@ namespace SOS.AndrewsAdventure.Character
 
         public void Update() 
         {
-            Enemy.destination = TheParty.position;
+            Enemy.destination = transform.position;
             
             if (inbattle == true)
             {
-                TheParty.position = new Vector3(-3f, 2.31f, 102f);
+                TheOriginalAndrew.position = new Vector3(-3f, 2.31f, 102f);
                 TheFirstOpponent.position = new Vector3(3f, 2.31f, 102f);
-                Lateef.position = new Vector3(-4f, 2.31f, 100f);
-                Meresankh.position = new Vector3(-5f, 2.31f, 98);
+                Lateef.position = new Vector3(-4f, 1f, 100f);
+                Meresankh.position = new Vector3(-5f, 1f, 98);
                 MainCamera.enabled = false;
                 BattleCamera.enabled = true;
             }
