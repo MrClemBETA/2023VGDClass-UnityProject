11fd66a6f2fee08faa146021b7195f7a2f6a34ff
Anthony Clemetson <aclemetson@cousd.net>
2024-03-08 09:59:35 -0800

    Started working on the damage taking math.

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -5,6 +5,12 @@ namespace SOS.AndrewsAdventure.Character
     [RequireComponent (typeof(Health))]
     public class Character : MonoBehaviour
     {
+        [SerializeField] float attackMultiplier;
 
+
+        public void TakeDamage(Character enemy)
+        {
+
+        }
     }
 }
