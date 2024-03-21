7091d15dfb01f99093cfc3259c0a00c0dd823873
Tyler Lynch <tlynch@cohsbusiness.edu>
2024-03-21 09:48:59 -0700

    Added the attribute groups to the characters

--- Assets/Scripts/Character/Party/Andrew.cs
+++ Assets/Scripts/Character/Party/Andrew.cs
@@ -6,24 +6,5 @@ using UnityEngine;
 public class Andrew : Character
 {
     public int HP = 10;
-    private float levelUpConstant = 1.02329299228f;
 
-    /*private void Start()
-    {
-        andrewStats.Add("maxHP", 10);
-        andrewStats.Add("AM", 1);
-        andrewStats.Add("BA", 0);
-        andrewStats.Add("DD", 1);
-        andrewStats.Add("BD", 0);
-    }*/
-
-    /*void levelUp(int level)
-    {
-        andrewStats.maxHP = Mathf.Round((maxHP * Mathf.Pow(levelUpConstant, (level - 1 / 2))));
-        andrewStats.Add(HP = maxHP;
-        AM += 0.01f;
-        BA += 0.1f;
-        DD += 0.01f;
-        BD += 0.1f;
-    }*/
 }
